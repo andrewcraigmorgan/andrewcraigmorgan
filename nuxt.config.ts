14601a7bafc565fab817497938f8ab5c1a9c55ee
Andrew Morgan <andrew@idesignwebsites.co.uk>
2024-12-01 12:30:44 +0000

    Push GitHub pages changes

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,11 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+    ssr: false,
+    target: "static",
     compatibilityDate: "2024-11-01",
     devtools: { enabled: true },
     css: ["@/assets/css/fonts.css"],
+    app: {
+        baseURL: "/andrewcraigmorgan/",
+    },
 });
