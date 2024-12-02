fba3db9a862d9f3538cfb6bb7901a2f15c930a60
Andrew Morgan <andrew@idesignwebsites.co.uk>
2024-12-02 21:09:19 +0000

    Turn SSR on

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,6 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-    ssr: false,
+    ssr: true,
     target: "static",
     compatibilityDate: "2024-11-01",
     devtools: { enabled: true },
