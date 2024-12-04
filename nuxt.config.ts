200858d4eab43ca294a2c96ed9607414c74513f6
Andrew Morgan <andrew@idesignwebsites.co.uk>
2024-12-04 00:21:25 +0000

    Add viewport meta

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,11 +3,11 @@ export default defineNuxtConfig({
     head: {
         meta: [
             {
-                name: 'viewport',
-                content: 'width=device-width, initial-scale=1'
-            }
-        ]
-    }
+                name: "viewport",
+                content: "width=device-width, initial-scale=1",
+            },
+        ],
+    },
     ssr: true,
     target: "static",
     compatibilityDate: "2024-11-01",
